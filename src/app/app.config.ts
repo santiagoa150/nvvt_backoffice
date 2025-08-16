4ba4ac9f9ba2767f9bb38f7eb58760ff009be8dc
Santiago Alvarez Muñoz <santiagoa150@gmail.com>
2025-08-15 19:14:29 -0500

    feat: internationalization with ngx translate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,23 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
+    provideTranslateService({
+      fallbackLang: 'es',
+      lang: 'es',
+      loader: provideTranslateHttpLoader({
+        prefix: '/i18n/',
+        suffix: '.json',
+      }),
+    }),
   ],
 };
--- src/app/auth/ui/pages/login/login.page.ts
+++ src/app/auth/ui/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 /**
  * This file defines the Login Page component.
@@ -8,5 +9,6 @@ import { Component } from '@angular/core';
   selector: 'app-login-page',
   templateUrl: './login.page.html',
   styleUrl: './login.page.scss',
+  imports: [TranslatePipe],
 })
 export class LoginPage {}
